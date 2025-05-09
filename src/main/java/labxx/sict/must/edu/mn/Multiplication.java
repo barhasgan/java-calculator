60d9926b0c090a189e056db892a174a767a11ddd
bargan123 <gansukhbarkhas@gmail.comm>
2025-05-09 17:44:38 +0800

    Initial push from local project

---
+++ src/main/java/labxx/sict/must/edu/mn/Multiplication.java
@@ -0,0 +1,8 @@
+package labxx.sict.must.edu.mn;
+
+public class Multiplication {
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+}
+//C:\Users\User\java-calculator\src\main\java\labxx\sict\must\edu\mn\Multiplication.java
---
+++ src/test/java/labxx/sict/must/edu/mn/AppTest.java
@@ -0,0 +1,12 @@
+package labxx.sict.must.edu.mn;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
